5bd281f38ea35e06f6879735a842d15f60069e8a
jrissler9 <jrissler9@gmail.com>
2018-04-17 20:24:21 -0400

    Added quotes.js file to use to build the side bar menu and content for the main display area. We modified the applicationInit function to sort the array. Modified the menuBuilder function to build the menu on the array. Modified the linkClicked function to display the quote when the side menu item is clicked. We made a few css entries and cleaned up the index file.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// JavaScript Document
+// Inside-Out Project Step-404
 'use strict';
 
 var appData = {
@@ -45,7 +45,7 @@ function initializeApplication() {
   elFooter.className = 'animated bounceInRight';
 
   elMain.innerHTML += '<div style="width: 40%; margin: auto; padding:10px; margin-bottom: 20px;"><div class="progress" style="height: 20px;"><div id="loaderProgressBar" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div></div></div>';
-
+    quotArr = quotArr.sort((a, b) => a[1].localeCompare(b[1]));
     displayPB();
 }
 
@@ -63,7 +63,6 @@ function displayPB() {
         timerCount = 0;
 
         document.body.innerHTML = formLogin();
-
         return false;
     }
 }
@@ -76,7 +75,6 @@ function validateLogin() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("pwd").value;
     if (email !== '' && password !== '') {
-        //document.body.innerHTML = '<main class="animated zoomIn"><h1 id="inNow">We are in now</h1></main>';
         applicationUserInterface();
     } else {
         alert('bad');
@@ -97,8 +95,14 @@ function applicationUserInterface() {
     }
 }
 
-function buildMenu() {
-    return '<nav class="sidebar animated slideInLeft"><ul class="nav flex-column"> <li class="nav-item"> <a class="nav-link active" data-dest="link01" href="#">Active</a> </li><li class="nav-item"> <a class="nav-link" data-dest="link02"  href="#">Link</a> </li><li class="nav-item"> <a class="nav-link" data-dest="link03" href="#">Link</a> </li><li class="nav-item"> <a class="nav-link disabled" data-dest="link04" href="#">Disabled</a> </li></ul></nav>';
+function  buildMenu() {
+  let sm = '<nav class="sidebar animated slideInLeft"><ul class="nav flex-column">';
+	     for (let i = 0; i < quotArr.length; i++) {
+	          sm += '<li class="nav-item"><a class="nav-link active" data-dest="' + i + '" href="#">' + quotArr[i][1].split(",", 1) + '</a></li>';
+	     }
+	     sm += '</ul></nav>';
+     return sm;
+
 }
 
 function buildMain() {
@@ -107,6 +111,6 @@ function buildMain() {
 
 function linkClicked(obj) {
     console.log(obj.dataset.dest);
-    var str = '<h1 class="animated zoomIn">' + obj.dataset.dest + ' was clicked!</h1>';
+    var str = '<div class="infoDiv"><h1 class="animated zoomIn">' + quotArr[obj.dataset.dest][0] + '</h1><div class="animated slideInRight auth">- ' + quotArr[obj.dataset.dest][1] + '</div></div>';
     document.getElementById("main").innerHTML = str.repeat(20);
 }
